c45cb7fafee4131163e58778b6f1657f4d88e5c4
EdinK1 <edinkaymakchi@gmail.com>
2019-07-25 21:19:41 -0700

    get ajax request

---
+++ js/ajax.js
@@ -0,0 +1,15 @@
+jQuery('#get-quotes').on('click', function(event) {
+  event.preventDefault();
+  jQuery.ajax({
+    method: 'GET',
+    url: red_vars.rest_url + 'wp/v2/posts/',
+    data: {
+      comment_status: 'closed'
+    },
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader('X-WP-Nonce', red_vars.wpapi_nonce);
+    }
+  }).done(function(response) {
+    console.log(response);
+  });
+});
