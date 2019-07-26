be8bfbaca5e0f77c6aec1a77e84a32a6f633afb6
EdinK1 <edinkaymakchi@gmail.com>
2019-07-26 01:32:52 -0700

    add the links

--- footer.php
+++ footer.php
@@ -11,6 +11,18 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
+				<ul>
+					<li>
+					<a href="<?= get_permalink( get_page_by_title( 'About' ) ); ?>">About </a> |
+					</li>
+					<li>
+					<a href="http://" target="_blank" rel="noopener noreferrer">Archives </a> |
+					</li>
+					<li>
+					<a href="http://" target="_blank" rel="noopener noreferrer">Submit a Quote</a>
+					</li>
+				</ul>
+				<p>Brought to you by <a href="https://edink1.github.io/">Edin Kaymakqi</a></p>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
