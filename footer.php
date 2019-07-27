36efdcbb5156ff4e61c6b3558b149eb48cb7e9c2
EdinK1 <edinkaymakchi@gmail.com>
2019-07-27 15:51:45 -0700

    add the menu

--- footer.php
+++ footer.php
@@ -11,17 +11,7 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-				<ul>
-					<li>
-					<a href="<?= get_permalink( get_page_by_title( 'About' ) ); ?>">About </a> |
-					</li>
-					<li>
-					<a href="http://" target="_blank" rel="noopener noreferrer">Archives </a> |
-					</li>
-					<li>
-					<a href="http://" target="_blank" rel="noopener noreferrer">Submit a Quote</a>
-					</li>
-				</ul>
+				<?php wp_nav_menu(array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>				
 				<p>Brought to you by <a href="https://edink1.github.io/">Edin Kaymakqi</a></p>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
