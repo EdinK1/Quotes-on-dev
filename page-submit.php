2f17c144e170434dc74bae2edc0c4278c05d77c0
EdinK1 <edinkaymakchi@gmail.com>
2019-07-29 22:52:50 -0700

    add the form

---
+++ page-submit.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package QOD_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			
+		<form id="submitForm" style="display: flex; flex-direction: column">
+			<label for="author">
+				Author of Quote
+				<input type="text" name="author" id="quote-author">
+			</label>
+			
+			<label for="content">
+				Quote
+				<textarea name="content" id="quote-content"></textarea>
+			</label>
+
+			<label for="source">
+				Where did you find this quote? (e.g book name)
+				<input type="text" name="source" id="quote-source">
+			</label>
+
+			<label for="url">
+				Provide the URL of the quote source, if available
+				<input type="url" name="url" id="quote-url">
+			</label>
+
+			<button type="submit">Submit Quote</button>
+		</form>						
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
