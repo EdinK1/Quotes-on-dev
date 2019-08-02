1815ddbe44c5c96374d14efb34e3a0f1238c4b11
EdinK1 <edinkaymakchi@gmail.com>
2019-08-01 20:07:43 -0700

    fix the comma issue

--- js/ajax.js
+++ js/ajax.js
@@ -1,11 +1,10 @@
 jQuery(() => {
-
   const prevPage = () => {
     window.location = window.location;
-  }
+  };
 
   window.addEventListener('popstate', prevPage);
-  
+
   // const anotherOne = () => {
   //   setTimeout(function() {
   //     jQuery('.preload').hide();
@@ -13,28 +12,39 @@ jQuery(() => {
   //   jQuery('.preload').show();
   // }
 
+  jQuery('#get-quotes').on('click', function(e) {
+    e.preventDefault();
 
-jQuery('#get-quotes').on('click', function(e) {
-  e.preventDefault();
-
-  jQuery.ajax({
-    method: 'GET',
-    url: red_vars.rest_url + 'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1',
-    data: {
-      comment_status: 'closed'
-    },
-    beforeSend: function(xhr) {
-      xhr.setRequestHeader('X-WP-Nonce', red_vars.wpapi_nonce);
-    }
-  }).done(function(response) {
-    history.pushState('', '', response[0].link)
-      for(let i = 0; i < response.length; i++) {
-        jQuery('article').empty().append(
-        `
+    jQuery
+      .ajax({
+        method: 'GET',
+        url:
+          red_vars.rest_url +
+          'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+        data: {
+          comment_status: 'closed'
+        },
+        beforeSend: function(xhr) {
+          xhr.setRequestHeader('X-WP-Nonce', red_vars.wpapi_nonce);
+        }
+      })
+      .done(function(response) {
+        history.pushState('', '', response[0].link);
+        for (let i = 0; i < response.length; i++) {
+          jQuery('.entry-content')
+            .empty()
+            .append(
+              `
         <p>${response[i].content.rendered}</p>
-        <h3> - ${response[i].title.rendered},
-        <a href="${response[i]._qod_quote_source_url}"> ${response[i]._qod_quote_source}</a></h3>
-       `);
+        <h3> - ${response[i].title.rendered}
+        ${response[i]._qod_quote_source_url &&
+          `, 
+        <a href="${response[i]._qod_quote_source_url}"> ${
+            response[i]._qod_quote_source
+          }</a></h3>
+        `}
+        `
+            );
         }
       });
   });
@@ -43,27 +53,30 @@ jQuery('#get-quotes').on('click', function(e) {
 jQuery('#submitForm').on('submit', function(e) {
   e.preventDefault();
 
-  let title                 = jQuery('#quote-author').val(),
-      content               = jQuery('#quote-content').val(),
-      _qod_quote_source     = jQuery('#quote-source').val(),
-      _qod_quote_source_url = jQuery('#quote-url').val();
+  let title = jQuery('#quote-author').val(),
+    content = jQuery('#quote-content').val(),
+    _qod_quote_source = jQuery('#quote-source').val(),
+    _qod_quote_source_url = jQuery('#quote-url').val();
 
-  jQuery.ajax({
-    method: 'POST',
-    url: red_vars.rest_url + 'wp/v2/posts',
-    data: {
-      title,
-      content,
-      _qod_quote_source,
-      _qod_quote_source_url,
-      status: 'pending'
-    },
-    beforeSend: function(xhr) {
-      xhr.setRequestHeader('X-WP-Nonce', red_vars.wpapi_nonce);
-    }
-    }).done(function() {
+  jQuery
+    .ajax({
+      method: 'POST',
+      url: red_vars.rest_url + 'wp/v2/posts',
+      data: {
+        title,
+        content,
+        _qod_quote_source,
+        _qod_quote_source_url,
+        status: 'pending'
+      },
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader('X-WP-Nonce', red_vars.wpapi_nonce);
+      }
+    })
+    .done(function() {
       console.log('done');
-  }).fail(function() {
+    })
+    .fail(function() {
       err => console.error(err);
-  })
-  });
+    });
+});
