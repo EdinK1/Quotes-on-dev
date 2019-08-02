537bc0ed44fd26df39fc3952610716cb13011868
EdinK1 <edinkaymakchi@gmail.com>
2019-08-01 21:09:38 -0700

    change code order

--- home.php
+++ home.php
@@ -9,19 +9,20 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		
-			<?php get_template_part( 'template-parts/content', 'home' ); ?>
-			<button type="button" class="get-quotes" id="get-quotes">Another One!</button>
-
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<main class="entry-content quotes">			
+				<?php the_content('<p>', '</p>'); ?>
+				<?php the_title('<h3>- ', '</h3>'); ?>
+			</main><!-- .entry-content -->
+		</article><!-- #post-## -->	
+		<button type="button" class="get-quotes" id="get-quotes">Another One!</>						
 			<?php endwhile; ?>
 			<?php else : ?>
 
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
 			<?php endif; wp_reset_query(); ?>
+			</div>
 		</main><!-- #main -->
-	</div><!-- #primary -->
 
 <?php get_footer(); ?>
